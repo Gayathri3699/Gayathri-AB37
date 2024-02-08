ac2a23d4c8b167c8b8c5d805d794bdcf3177b700
Gayathri <archana1301994@gmailcom>
2024-02-08 12:29:36 +0530

    String Functions

---
+++ Practice.java
@@ -0,0 +1,27 @@
+package stringfunctions;
+
+import java.util.Date;
+
+public class Practice 
+{
+
+	public static void main(String[] args)
+	{
+      String name = "rahul";
+      for(int i= 4;i>=0;i--)
+   {
+	   System.out.println(name.charAt(i));
+   }
+		/*Date d1 = new Date();
+		System.out.println(d1.getTime());
+		Date d2 = new Date(d1.getTime());
+		System.out.println(d2);*/
+		/*String name = "my name is archana";
+		String replace = name.replaceAll("[a-z]", "");
+		
+		System.out.println(replace);
+		String s2[] = name.split(" ");
+		System.out.println(s2[3]);*/
+	}
+
+}
---
+++ Program.java
@@ -0,0 +1,12 @@
+package stringfunctions;
+
+public class Program 
+{
+
+	public static void main(String[] args) 
+	{
+       String name = "my name is kiran";
+  
+	}
+
+}
